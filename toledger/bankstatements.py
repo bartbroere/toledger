fa11d35224ea31ce71901fb6eaabcf70c446c34b
bartbroere <broerebart@hotmail.com>
2017-04-21 11:41:55 +0200

    Bitstamp plan

--- toledger/bankstatements.py
+++ toledger/bankstatements.py
@@ -2,6 +2,38 @@
 Contains metadata for parsing input files
 """
 
+#2017-04-21 13:21 Market
+#    Assets:Bitstamp              10 BTC @@ $5000.00
+#    Assets:Bitstamp
+
+#2017-04-21 14:21 Deposit
+#    Assets:Bitstamp              10 BTC
+#    Equity:Unspecified
+
+bitstamp_net = {"transactiontypes": "Type",
+                "Withdrawal": {},
+                "Market": {},
+                "Sub Account Transfer": {},
+                "Deposit": {}}
+
+bitstamp_net = {"fields": {"Type": ["name", "direction"],
+                           "Datetime": "date",
+                           "Account": "meta",
+                           "Amount": "amount",
+                           "Value": "value",
+                           "Rate": "meta",
+                           "Fee": "meta",
+                           "Sub Type": "direction"},
+                "interpretation": {"byfield": "Type",
+                                   "fields": {"Market": "assetconv",
+                                              "Deposit": "default",
+                                              "Withdrawal": "default",
+                                              "Sub Account Transfer": "subaccount"}
+                                   "directionout": ["Sell", "Withdrawal"],
+                                   "dateformat": ""},
+                "properties": {"quotechar": "\"",
+                               "delimiter": ","}}
+
 ing_nl = {"fields": {"Datum": "date",
                      "Naam / Omschrijving": "name",
                      "Rekening": "source",
@@ -18,4 +50,17 @@ ing_nl = {"fields": {"Datum": "date",
                                             "decimalseparator": ","},
                              "dateformat": "%Y%m%d"},
           "properties": {"quotechar": "\"",
-                         "delimiter": ","}}
+                         "delimiter": ","},
+          "type": "default"}
+
+kraken_com = {"group_by": "refid"
+              "fields": {"time": "date",
+                         "refid": "name",
+                         "txid": "source",
+                         "amount": "amount",
+                         "type": "meta",
+                         "aclass": "meta",
+                         "fee": "meta",
+                         "balance": "balance"},
+              "interpretation": {},
+              "properties": {}}
